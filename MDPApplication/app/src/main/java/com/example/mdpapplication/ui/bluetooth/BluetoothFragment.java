b9c7a5a6dfc06e85ed94bfeae29f77484075856d
Zhilin <huangzhilin1999@gmail.com>
2021-01-30 01:06:16 +0800

    Change Gallery to Bluetooth

--- MDPApplication/app/src/main/java/com/example/mdpapplication/ui/bluetooth/BluetoothFragment.java
+++ MDPApplication/app/src/main/java/com/example/mdpapplication/ui/bluetooth/BluetoothFragment.java
@@ -1,4 +1,4 @@
-package com.example.mdpapplication.ui.gallery;
+package com.example.mdpapplication.ui.bluetooth;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -14,17 +14,17 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.example.mdpapplication.R;
 
-public class GalleryFragment extends Fragment {
+public class BluetoothFragment extends Fragment {
 
-    private GalleryViewModel galleryViewModel;
+    private BluetoothViewModel bluetoothViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        galleryViewModel =
-                new ViewModelProvider(this).get(GalleryViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_gallery, container, false);
-        final TextView textView = root.findViewById(R.id.text_gallery);
-        galleryViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        bluetoothViewModel =
+                new ViewModelProvider(this).get(BluetoothViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_bluetooth, container, false);
+        final TextView textView = root.findViewById(R.id.text_bluetooth);
+        bluetoothViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
--- MDPApplication/app/src/main/java/com/example/mdpapplication/ui/bluetooth/BluetoothViewModel.java
+++ MDPApplication/app/src/main/java/com/example/mdpapplication/ui/bluetooth/BluetoothViewModel.java
@@ -1,16 +1,16 @@
-package com.example.mdpapplication.ui.gallery;
+package com.example.mdpapplication.ui.bluetooth;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class GalleryViewModel extends ViewModel {
+public class BluetoothViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public GalleryViewModel() {
+    public BluetoothViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is gallery fragment");
+        mText.setValue("This is bluetooth fragment");
     }
 
     public LiveData<String> getText() {
