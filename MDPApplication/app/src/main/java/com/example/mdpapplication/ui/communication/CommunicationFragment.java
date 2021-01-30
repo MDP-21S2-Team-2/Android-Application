e84a3f1f844b1e4d51ef4e273ca785bc710cf35d
Zhilin <huangzhilin1999@gmail.com>
2021-01-30 21:19:40 +0800

    Implement logic for persistent text strings

--- MDPApplication/app/src/main/java/com/example/mdpapplication/ui/communication/CommunicationFragment.java
+++ MDPApplication/app/src/main/java/com/example/mdpapplication/ui/communication/CommunicationFragment.java
@@ -1,5 +1,7 @@
 package com.example.mdpapplication.ui.communication;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,29 +9,55 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.mdpapplication.R;
 
+
 public class CommunicationFragment extends Fragment {
 
+    private static final String PERSISTENT_STRING_KEY_1 = "persistent_string_1";
+    private static final String PERSISTENT_STRING_KEY_2 = "persistent_string_2";
+    private static final String PERSISTENT_STRING_DEFAULT_1 = "This is persistent text string 1";
+    private static final String PERSISTENT_STRING_DEFAULT_2 = "This is persistent text string 2";
+
     private CommunicationViewModel communicationViewModel;
+    private TextView textViewCommunicationString1;
+    private TextView textViewCommunicationString2;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         communicationViewModel =
                 new ViewModelProvider(this).get(CommunicationViewModel.class);
         View root = inflater.inflate(R.layout.fragment_communication, container, false);
-//        final TextView textView = root.findViewById(R.id.editTextCommunicationString1);
-//        communicationViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-//            @Override
-//            public void onChanged(@Nullable String s) {
-//                textView.setText(s);
-//            }
-//        });
+        textViewCommunicationString1 = root.findViewById(R.id.editTextCommunicationString1);
+        textViewCommunicationString2 = root.findViewById(R.id.editTextCommunicationString2);
         return root;
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        // Write persistent strings in SharedPreferences
+        SharedPreferences sharedPreferences = getActivity().getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(PERSISTENT_STRING_KEY_1, "" + textViewCommunicationString1.getText());
+        System.out.println("Saved to shared preferences: " + textViewCommunicationString1.getText());
+        editor.putString(PERSISTENT_STRING_KEY_2, "" + textViewCommunicationString2.getText());
+        editor.commit();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        // Get persistent strings from SharedPreferences
+        SharedPreferences sharedPreferences = getActivity().getPreferences(Context.MODE_PRIVATE);
+        String communicationStringValue1 = sharedPreferences.getString(PERSISTENT_STRING_KEY_1, PERSISTENT_STRING_DEFAULT_1);
+        String communicationStringValue2 = sharedPreferences.getString(PERSISTENT_STRING_KEY_2, PERSISTENT_STRING_DEFAULT_2);
+        textViewCommunicationString1.setText(communicationStringValue1);
+        textViewCommunicationString2.setText(communicationStringValue2);
     }
 }
