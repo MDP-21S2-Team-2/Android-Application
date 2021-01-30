0d6a0603f84467de07db840afb8283e3e9c2b38f
Zhilin <huangzhilin1999@gmail.com>
2021-01-31 00:02:52 +0800

    Add my devices list for bluetooth UI

--- MDPApplication/app/src/main/java/com/example/mdpapplication/ui/bluetooth/BluetoothFragment.java
+++ MDPApplication/app/src/main/java/com/example/mdpapplication/ui/bluetooth/BluetoothFragment.java
@@ -4,12 +4,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.mdpapplication.R;
@@ -17,19 +16,23 @@ import com.example.mdpapplication.R;
 public class BluetoothFragment extends Fragment {
 
     private BluetoothViewModel bluetoothViewModel;
+    private ListView myDevicesListView;
+
+    private ArrayAdapter<String> myDevicesArrayAdapter;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         bluetoothViewModel =
                 new ViewModelProvider(this).get(BluetoothViewModel.class);
         View root = inflater.inflate(R.layout.fragment_bluetooth, container, false);
-//        final TextView textView = root.findViewById(R.id.text_bluetooth);
-//        bluetoothViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-//            @Override
-//            public void onChanged(@Nullable String s) {
-//                textView.setText(s);
-//            }
-//        });
+        myDevicesListView = root.findViewById(R.id.myDevicesListView);
+
+        // Set adapter for myDevicesArrayAdapter
+        myDevicesArrayAdapter = new ArrayAdapter<String>(this.getContext(), R.layout.device_name);
+        myDevicesListView.setAdapter(myDevicesArrayAdapter);
+//        myDevicesArrayAdapter.add("Test Device 1");
+//        myDevicesArrayAdapter.add("Test Device 2");
+
         return root;
     }
 }
