24a6f8a371d4785eb4dfc1d21bb1e270bde84a36
Zhilin <huangzhilin1999@gmail.com>
2021-01-31 00:15:26 +0800

    Add refresh my devices button for bluetooth UI

--- MDPApplication/app/src/main/java/com/example/mdpapplication/ui/bluetooth/BluetoothFragment.java
+++ MDPApplication/app/src/main/java/com/example/mdpapplication/ui/bluetooth/BluetoothFragment.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 import androidx.annotation.NonNull;
@@ -17,6 +18,8 @@ public class BluetoothFragment extends Fragment {
 
     private BluetoothViewModel bluetoothViewModel;
     private ListView myDevicesListView;
+    private Button enableBluetoothButton;
+    private Button refreshMyDevicesButton;
 
     private ArrayAdapter<String> myDevicesArrayAdapter;
 
@@ -26,13 +29,27 @@ public class BluetoothFragment extends Fragment {
                 new ViewModelProvider(this).get(BluetoothViewModel.class);
         View root = inflater.inflate(R.layout.fragment_bluetooth, container, false);
         myDevicesListView = root.findViewById(R.id.myDevicesListView);
+        enableBluetoothButton = root.findViewById(R.id.enableBluetoothButton);
+        refreshMyDevicesButton = root.findViewById(R.id.refreshMyDevicesButton);
 
         // Set adapter for myDevicesArrayAdapter
-        myDevicesArrayAdapter = new ArrayAdapter<String>(this.getContext(), R.layout.device_name);
+        myDevicesArrayAdapter = new ArrayAdapter<>(this.getContext(), R.layout.device_name);
         myDevicesListView.setAdapter(myDevicesArrayAdapter);
         // TODO: Remove dummy devices
         myDevicesArrayAdapter.add("Dummy Device 1");
         myDevicesArrayAdapter.add("Dummy Device 2");
+
+        enableBluetoothButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // TODO: Enable discovery by other bluetooth devices using BluetoothAdapter
+            }
+        });
+
+        refreshMyDevicesButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // TODO: Refresh my devices list
+            }
+        });
 
         return root;
     }
