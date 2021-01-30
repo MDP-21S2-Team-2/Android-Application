f9adc8a061a0cd6f918c47e62fa1062a20b91f49
Zhilin <huangzhilin1999@gmail.com>
2021-01-30 23:50:26 +0800

    Add enable bluetooth button for bluetooth UI

--- MDPApplication/app/src/main/java/com/example/mdpapplication/ui/bluetooth/BluetoothFragment.java
+++ MDPApplication/app/src/main/java/com/example/mdpapplication/ui/bluetooth/BluetoothFragment.java
@@ -23,13 +23,13 @@ public class BluetoothFragment extends Fragment {
         bluetoothViewModel =
                 new ViewModelProvider(this).get(BluetoothViewModel.class);
         View root = inflater.inflate(R.layout.fragment_bluetooth, container, false);
-        final TextView textView = root.findViewById(R.id.text_bluetooth);
-        bluetoothViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+//        final TextView textView = root.findViewById(R.id.text_bluetooth);
+//        bluetoothViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+//            @Override
+//            public void onChanged(@Nullable String s) {
+//                textView.setText(s);
+//            }
+//        });
         return root;
     }
 }
